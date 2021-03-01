0a7f4f3e08eb59dec447e1c6353cb4803780133e
Alex G <ulledar@ua.fm>
2021-03-01 15:25:18 +0200

    comment update

--- src/test/java/CSSTest.java
+++ src/test/java/CSSTest.java
@@ -2,12 +2,12 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 public class CSSTest extends BaseTest {
-    String searchField = "input#query";
-    String searchButton = "input.button";
-    String headerJUnit = "h2 a[href$='junit-jupiter-api']";
-    //String choseVersion = "a.vbtn.milestone";
-    String choseVersion = "a[href='junit-jupiter-api/5.8.0-M1']";
-    String copyField = "textarea.snippet";
+    String searchField = "input#query"; //айди "квери" во вкладке инпут
+    String searchButton = "input.button"; //класс "батон" во вкладке "инпут"
+    String headerJUnit = "h2 a[href$='junit-jupiter-api']"; //ссылка заканчивается на "junit-jupiter-api" во вкладке "а" в "аш2"
+    //String choseVersion = "a.vbtn.milestone"; //класс "vbtn milestone" во складке "а"
+    String choseVersion = "a[href='junit-jupiter-api/5.8.0-M1']"; //ссылка "junit-jupiter-api/5.8.0-M1" во вкладке "а"
+    String copyField = "textarea.snippet"; //класс "сниппет" во складке "текстареа"
 
     @Test
     public void findJUnit() {
--- src/test/java/XpathTest.java
+++ src/test/java/XpathTest.java
@@ -2,9 +2,9 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 public class XpathTest extends BaseTest {
-    String searchField = "//input[@class='textfield']";
+    String searchField = "//input[@class='textfield']"; //класс "textfield" во вкладке инпут
     String searchButton = "//input[@class='button']";
-    String seleniumHeader = "//a[contains(text(),'Selenium Java')]";
+    String seleniumHeader = "//a[contains(text(),'Selenium Java')]"; //во вкладке "а" присутствует "Селениум Джава"
     String seleniymTitle = "Maven Repository: org.seleniumhq.selenium » selenium-java";
 
     @Test
@@ -12,7 +12,7 @@ public class XpathTest extends BaseTest {
         driver.findElement(By.xpath(searchField)).sendKeys("selenium");
         driver.findElement(By.xpath(searchButton)).click();
         driver.findElement(By.xpath(seleniumHeader)).click();
-        driver.getTitle().equals(seleniymTitle);
+        driver.getTitle().equals(seleniymTitle); //сравниваем тайтл с ожидаемым тайтлом
         System.out.println("Selenium finder finished his work.");
     }
 }
